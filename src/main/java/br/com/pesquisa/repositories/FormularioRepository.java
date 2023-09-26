3d41f496966bd68ccac395dbe7bfd6d93aa9adbe
FirminoAndrade <firmino_andrade@yahoo.com.br>
2023-09-26 10:26:21 -0300

    melhoria dos metodos do controller Formulario

--- src/main/java/br/com/pesquisa/repositories/FormularioRepository.java
+++ src/main/java/br/com/pesquisa/repositories/FormularioRepository.java
@@ -18,7 +18,7 @@ public interface FormularioRepository extends JpaRepository<Formulario, Long>{
 	List<Formulario> findByIdFormulario(@Param(value = "id_formulario") Long id_formulario);
 
 	@JsonValue
-	@Query(nativeQuery = true, value = "SELECT count(id) FROM FORMULARIO WHERE  PESQUISA_ID = :id and OPCAO_VOTO like /*#nomeCandidato*/;")
+	@Query(nativeQuery = true, value = "SELECT count(id) FROM FORMULARIO WHERE  PESQUISA_ID = :id and OPCAO_VOTO like = :nomeCandidato;")
 	int findByQtdVotoPorCandidato(@Param(value = "id") Long id, @Param(value = "nomeCandidato") String nomeCandidato);
 
 	@Query(nativeQuery = true, value = "SELECT count(id) FROM FORMULARIO WHERE  PESQUISA_ID = :id and OPCAO_VOTO is null;")
